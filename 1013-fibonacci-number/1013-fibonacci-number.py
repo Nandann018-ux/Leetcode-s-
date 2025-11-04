f70d982e95e777106fea2748bfeb878c8079f921
Nandan Acharya <samarthachar18@gmail.com>
2025-11-05 00:13:00 +0530

    Time: 16 ms (65.89%), Space: 12.4 MB (51.07%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -4,11 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 0 :
-            return 0 
-        if n == 1 :
-            return 1
-        
+        memo = {0:0,1:1}
 
-        return self.fib(n-2) + self.fib(n-1)
+        def f(x):
+            if x in memo:
+                return memo[x]
+            else:
+                memo[x] = f(x-1) + f(x-2)
+                return memo[x]
         
+        return f(n)
