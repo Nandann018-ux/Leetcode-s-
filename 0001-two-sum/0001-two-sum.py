a9c426b771d11a946db5ce9cad987abe9c121986
Nandan <nandanachar18@gmail.com>
2026-04-25 15:51:36 +0530

    Time: 4 ms (48.25%), Space: 12.8 MB (95.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,8 +5,10 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range (len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return[i,j]
-        return []
+        pre = {}
+
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in pre:
+                return [pre[diff],i]
+            pre[n] = i
